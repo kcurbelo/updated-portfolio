507d803dcdd92c4c8e50cf113828a8d7747cc1e1
Kevin Curbelo <kevin.curbelo@gmail.com>
2017-12-31 11:56:29 -0800

    Added jquery animations

--- js/lead.js
+++ js/lead.js
@@ -1,13 +1,51 @@
+$( document ).ready(function() {
+  
+  // Function to run when window is loaded
+  $( ".work-content" ).first().show( "normal", function showNext() {
+      $( this ).next( ".work-content" ).show( "normal", showNext );
+      console.log(this);         
+  });
+  $( ".work-content:last" ).addClass( "selected");
 
-  $( "#showr" ).click(function() {
-    $( ".jo" ).first().show( "fast", function showNext() {
-      $( this ).next( ".jo" ).show( "fast", showNext );
+});
+
+// Work link function
+// function workFunction(){
+  $( ".work-link" ).click(function() {
+    // Shows work content
+    $( ".work-content" ).first().show( "fast", function showNext() {
+      $( this ).next( ".work-content" ).show( "fast", showNext );
     });
+    // Hides about content
+    $( ".about-content" ).hide( "fast" );
+    // Hides contact content
+    $( ".contact-content" ).hide( "fast" );
   });
-   
-  $( "#hidr" ).click(function() {
-    $( ".jo" ).first().hide( "fast", function hideNext() {
-      $( this ).next( ".jo" ).hide( "fast", hideNext );
-    });
+// }
+
+// function aboutFunction(){
+  // About link function
+  $( ".about-link" ).click(function() {
+    // Shows about content
+    $( ".about-content" ).show( "slow" );
+    // Hides work content
+    $( ".work-content" ).hide( "fast" );
+    // Hides contact content
+    $( ".contact-content" ).hide( "slow" );
   });
+// }
+
+// function contactFunction(){
+  // Contact link function
+  $( ".contact-link" ).click(function() {
+    // Shows about content
+    $( ".contact-content" ).show( "slow" );
+    // Hides about content
+    $( ".about-content" ).hide( "slow" ); 
+    // Hides work content
+    $( ".work-content" ).hide( "fast" );
+  });
+// }
+
+
 
