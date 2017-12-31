60d6731c940d533073f41defcdae31082fb9c5be
Kevin Curbelo <kevin.curbelo@gmail.com>
2017-12-31 13:10:30 -0800

    Fixed jqeury show problem

--- js/lead.js
+++ js/lead.js
@@ -1,21 +1,16 @@
-$( document ).ready(function() {
-  
-  // Function to run when window is loaded
-  $( ".work-content" ).first().show( "normal", function showNext() {
-      $( this ).next( ".work-content" ).show( "normal", showNext );
-      console.log(this);         
-  });
-  $( ".work-content:last" ).addClass( "selected");
 
-});
+// Runs on page load
+$(".work-content").each(function(index) {
+    $(this).delay(200*index).fadeIn(200);
+}); 
 
 // Work link function
 // function workFunction(){
   $( ".work-link" ).click(function() {
     // Shows work content
-    $( ".work-content" ).first().show( "fast", function showNext() {
-      $( this ).next( ".work-content" ).show( "fast", showNext );
-    });
+    $(".work-content").each(function(index) {
+        $(this).delay(200*index).fadeIn(200);
+    }); 
     // Hides about content
     $( ".about-content" ).hide( "fast" );
     // Hides contact content
