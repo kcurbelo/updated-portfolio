5a11b849fef786603f69b5ccd68b8d2cd8f29ff9
Kevin Curbelo <kevin.curbelo@gmail.com>
2018-01-01 17:41:37 -0800

    Added css effects

--- js/lead.js
+++ js/lead.js
@@ -1,7 +1,7 @@
 
 // Runs on page load
 $(".work-content").each(function(index) {
-    $(this).delay(200*index).fadeIn(200);
+    $(this).delay(300*index).fadeIn(300);
 }); 
 
 // Work link function
@@ -9,12 +9,18 @@ $(".work-content").each(function(index) {
   $( ".work-link" ).click(function() {
     // Shows work content
     $(".work-content").each(function(index) {
-        $(this).delay(200*index).fadeIn(200);
+        $(this).delay(300*index).fadeIn(300);
     }); 
     // Hides about content
     $( ".about-content" ).hide( "fast" );
     // Hides contact content
     $( ".contact-content" ).hide( "fast" );
+    // Add active link class
+    $( ".work-link" ).addClass("active");
+    // Remove active link class for other link
+    $( ".about-link" ).removeClass("active");
+    // Remove active link class for other link
+    $( ".contact-link" ).removeClass("active");    
   });
 // }
 
@@ -22,11 +28,17 @@ $(".work-content").each(function(index) {
   // About link function
   $( ".about-link" ).click(function() {
     // Shows about content
-    $( ".about-content" ).show( "slow" );
+    $( ".about-content" ).slideDown( "slow" );
     // Hides work content
     $( ".work-content" ).hide( "fast" );
     // Hides contact content
     $( ".contact-content" ).hide( "slow" );
+    // Add active link class
+    $( ".work-link" ).removeClass("active");
+    // Remove active link class for other link
+    $( ".about-link" ).addClass("active");
+    // Remove active link class for other link
+    $( ".contact-link" ).removeClass("active");        
   });
 // }
 
@@ -34,11 +46,17 @@ $(".work-content").each(function(index) {
   // Contact link function
   $( ".contact-link" ).click(function() {
     // Shows about content
-    $( ".contact-content" ).show( "slow" );
+    $( ".contact-content" ).slideDown( "slow" );
     // Hides about content
     $( ".about-content" ).hide( "slow" ); 
     // Hides work content
     $( ".work-content" ).hide( "fast" );
+    // Add active link class
+    $( ".work-link" ).removeClass("active");
+    // Remove active link class for other link
+    $( ".about-link" ).removeClass("active");
+    // Remove active link class for other link
+    $( ".contact-link" ).addClass("active");            
   });
 // }
 
